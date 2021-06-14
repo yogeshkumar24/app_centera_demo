24dd914c667a65d8bcbf22bbd90c2be9ce0b928b
yogeshkumar24 <yogesh.kumar.official97@gmail.com>
2021-06-14 10:09:53 +0530

    local property add

--- lib/main.dart
+++ lib/main.dart
@@ -26,10 +26,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
