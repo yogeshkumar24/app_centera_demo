208eae1878681aa1c2940194f22b5e7fa16c564c
yogeshkumar24 <yogesh.kumar.official97@gmail.com>
2021-06-14 10:16:30 +0530

    fix errro

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
